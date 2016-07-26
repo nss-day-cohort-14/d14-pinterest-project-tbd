738844fc54182a8ae5acdefd80ba874067650f2f
Thomas Swann <tkswann@mac.com>
2016-07-26 11:36:04 -0500

    auth w/email working

--- app/controllers/loginCtrl.js
+++ app/controllers/loginCtrl.js
@@ -11,14 +11,35 @@ app.controller('loginCtrl', function ($scope, authFactory) {
 
   	console.log("Login initialized");
 
-  	authFactory.authWithProvider(authFactory.googleProvider)
+    authFactory.authWithProvider(authFactory.googleProvider)
     .then(function(result) {
       var user = result.user.uid;
       console.log("logged in user", user);
     }).catch(function(error) {
       console.log(error);
     });
-  }
+  };
+
+  $scope.newEmail = function () {
+    authFactory.createWithEmail($scope.email, $scope.password)
+    .then(function(result) {
+      // var user = result.uid;
+      console.log("logged in user", result.uid);
+    })
+    .catch(function(err) {
+      console.log(error);
+    });
+  };
+
+  $scope.existingEmail = function () {
+    authFactory.authWithEmail($scope.email, $scope.password)
+    .then(function(result) {
+      console.log(result.uid);
+    })
+    .catch(function(err) {
+      console.log(err);
+    })
+  };
 
   $scope.logout = function(){
   	firebase.auth().signOut()
--- app/factories/authFactory.js
+++ app/factories/authFactory.js
@@ -1,22 +1,22 @@
 'use strict';
 
-app.factory('authFactory', function() {
+app.factory('authFactory', function($location) {
 
   let currentUserId = null;
   let googleProvider = new firebase.auth.GoogleAuthProvider();
 
-  //onAuthStateChanged function to keep current UserId set correctly. 
+  //onAuthStateChanged function to keep current UserId set correctly.
   //in the if set UID var to UID
   //in the else set UID var to null
   firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
       console.log("user logged in");
       currentUserId = user.uid;
-      // $location.url("/start");
+      $location.url("/boards");
     } else {
       console.log("user not logged in");
       currentUserId = null;
-      // $location.url("/login");
+      $location.url("/login");
     }
   });
 
@@ -35,8 +35,27 @@ app.factory('authFactory', function() {
     return currentUserId;
   };
 
+  let createWithEmail = function (email, password) {
+    return firebase.auth().createUserWithEmailAndPassword(email, password)
+    .catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+    });
+  };
+
+  let authWithEmail = function (email, password) {
+    return firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+    });
+  };
+
   return {
-    authWithProvider, isAuthenticated, getUser, googleProvider
+    authWithProvider, isAuthenticated, getUser, googleProvider, createWithEmail, authWithEmail
   };
 
 });
