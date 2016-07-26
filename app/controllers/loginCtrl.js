f8aaceb7729872056e87cc5fba7ae2e2fc2b38fc
Marshall Friskics-Warren <wmfwarren@gmail.com>
2016-07-26 09:59:59 -0500

    added login and logout base functions for google auth

--- app/controllers/loginCtrl.js
+++ app/controllers/loginCtrl.js
@@ -1,9 +1,30 @@
 'use strict';
 
-app.controller('loginCtrl', function ($scope) {
-  $scope.openModal = function () {
-    $('#modal1').openModal();
-  };
+app.controller('loginCtrl', function ($scope, authFactory) {
+  
+  $scope.login = function(){
+
+  	console.log("Login initialized");
+
+  	authFactory.authWithProvider()
+    .then(function(result) {
+      var user = result.user.uid;
+      console.log("logged in user", user);
+    }).catch(function(error) {
+      console.log(error);
+    });
+  }
+
+  $scope.logout = function(){
+  	firebase.auth().signOut().then(function() {
+		  // Sign-out successful.
+		  authFactory.currentUserID = null;
+		  console.log(authFactory.getUser(), "Logged out");
+		}, function(error) {
+		  // An error happened.
+		  console.log(error);
+		});
+  }
 
   $scope.openModal();
 });
