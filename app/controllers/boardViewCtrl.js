e0d93b3459036f7eac6737c7936a79cf9683803c
Jack Mocherman <mochermanjack@gmail.com>
2016-07-26 11:44:34 -0500

    added boardArray and deleteBoard function to , set get data function to store return data in boardArray

--- app/controllers/boardViewCtrl.js
+++ app/controllers/boardViewCtrl.js
@@ -1,14 +1,20 @@
 'use strict';
 
-app.controller('boardView', function ($scope, dataFactory) {
+app.controller('boardView', function($scope, dataFactory) {
 
-	var board = true; //this tell the getData() that it is looking for boards
-	//board object array
-	//delete board
+  var board = true; //this tell the getData() that it is looking for boards
+  $scope.boardArray = []; //board object array
 
-	dataFactory.getData(board)
-	.then((data) => {
-	}
-	);
+  //delete board
+  $scope.deleteBoard = function() {
+    console.log("clicked delete");
+  };
+
+  dataFactory.getData(board)
+    .then((boardCollection) => {
+      console.log("", boardCollection);
+      $scope.boardArray = boardCollection;
+    });
+  dataFactory.getData();
 
 });
