b189a70a86440eada881a0d182042b92012904ba
Marshall Friskics-Warren <wmfwarren@gmail.com>
2016-07-26 11:01:36 -0500

    added dummy objec to controllers

--- app/controllers/newBoardCtrl.js
+++ app/controllers/newBoardCtrl.js
@@ -1,5 +1,11 @@
 'use strict';
 app.controller('newBoardCtrl', function ($scope) {
-
+	$scope.newBoard = {
+		description: "",
+		refKey: "",
+		tags: [],
+		title: "",
+		uid: ""
+	};
 
 });
--- app/controllers/newPinCtrl.js
+++ app/controllers/newPinCtrl.js
@@ -1,5 +1,13 @@
 'use strict';
 app.controller('newPinCtrl', function ($scope) {
 
+	$scope.newBoard = {
+		boardKey: "",
+		comments: "",
+		refKey: "",
+		tags: [],
+		title: "",
+		url: ""
+	};
 
 });
