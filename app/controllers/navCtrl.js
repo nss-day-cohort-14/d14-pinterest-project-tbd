14caa2aa556f13337e9e9466658cbea080857fd0
Marshall Friskics-Warren <wmfwarren@gmail.com>
2016-07-27 01:30:25 -0500

    made changes then reverted

--- app/controllers/navCtrl.js
+++ app/controllers/navCtrl.js
@@ -2,8 +2,6 @@
 
 app.controller('navCtrl', function ($scope, authFactory) {
 
-	//scoped boolean to see if logged in or logged out and determine which
-	////log state button we need to show
 	$scope.dummy = function () {
 		$scope.user = authFactory.getUser();
 		console.log($scope.user, 'data RAWR');
